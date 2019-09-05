5a3c7ed877b39c209e1c61c520b04598cadcece1
20150853 CUEVAS ZEPEDA ALEJANDRO <20150853@TELEMATICA.UCOL.MX>
2019-09-05 14:27:12 -0500

    Correccion

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class Competidores{
         if(avanceTortuga <= 45){
             this._tortuga += 3;
 
-        }else if(avanceTortuga >= 46 || avanceTortuga <= 63){
+        }else if(avanceTortuga >= 46 && avanceTortuga <= 63){
             this._tortuga -= 6;
 
         }else{
@@ -25,13 +25,13 @@ class Competidores{
         if(avanceLiebre <= 18){
             this._liebre += 0;
 
-        }else if(avanceLiebre >= 19 || avanceLiebre <= 36){
+        }else if(avanceLiebre >= 19 && avanceLiebre <= 36){
             this._liebre += 9;
 
-        }else if(avanceLiebre >= 37 || avanceLiebre <= 45){
+        }else if(avanceLiebre >= 37 && avanceLiebre <= 45){
             this._liebre -= 12;
 
-        }else if(avanceLiebre >= 46 || avanceLiebre <= 76){
+        }else if(avanceLiebre >= 46 && avanceLiebre <= 76){
             this._liebre += 1;
         
         }else{
@@ -45,7 +45,7 @@ let nuevaCarrera = new Competidores();
 resultadoCarrera();
 
 function resultadoCarrera (){
-    for(var i=0; i<100; i++){
+    for(var i=0; i<110; i++){
         nuevaCarrera.tortugaAvance();
         nuevaCarrera.liebreAvance();
         console.log("Tortuga: "+nuevaCarrera._tortuga+" --- "+"Liebre: "+nuevaCarrera._liebre);
