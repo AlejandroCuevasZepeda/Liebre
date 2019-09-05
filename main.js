eafa97b7f108e721d1207a9ca03b1ccf655705af
20150853 CUEVAS ZEPEDA ALEJANDRO <20150853@TELEMATICA.UCOL.MX>
2019-09-05 15:14:15 -0500

    Herencia

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
-class Competidores{
+class Corredor{
     constructor(){
-        this._tortuga = 0;
-        this._liebre = 0;
-    }
+        this._posicion = 0;
+    }  
 
-    tortugaAvance(){
+    get posicion(){
+        return this._posicion;
+    }
+}
+
+class Tortuga extends Corredor{
+    avanzar(){
         let avanceTortuga = Math.trunc(Math.random()*90+1);
     
         if(avanceTortuga <= 45){
-            this._tortuga += 3;
+            this._posicion += 3;
 
         }else if(avanceTortuga >= 46 && avanceTortuga <= 63){
-            this._tortuga -= 6;
+            this._posicion -= 6;
 
         }else{
-            this._tortuga += 1;
-        }
-    }
-
-    liebreAvance(){
-
-        let avanceLiebre = Math.trunc(Math.random()*90+1);
-        
-        if(avanceLiebre <= 18){
-            this._liebre += 0;
-
-        }else if(avanceLiebre >= 19 && avanceLiebre <= 36){
-            this._liebre += 9;
-
-        }else if(avanceLiebre >= 37 && avanceLiebre <= 45){
-            this._liebre -= 12;
-
-        }else if(avanceLiebre >= 46 && avanceLiebre <= 76){
-            this._liebre += 1;
-        
-        }else{
-            this._liebre -= 2;
+            this._posicion += 1;
         }
     }
 }
 
+class Liebre extends Corredor{
+    avanzar(){
 
-let nuevaCarrera = new Competidores();
-resultadoCarrera();
+        let avanceLiebre = Math.trunc(Math.random()*90+1);
+        
+        if(avanceLiebre <= 18){
+            this._posicion += 0;
 
-function resultadoCarrera (){
-    for(var i=0; i<110; i++){
-        nuevaCarrera.tortugaAvance();
-        nuevaCarrera.liebreAvance();
-        console.log("Tortuga: "+nuevaCarrera._tortuga+" --- "+"Liebre: "+nuevaCarrera._liebre);
-        if(nuevaCarrera._tortuga >= 90 && nuevaCarrera._liebre >= 90){
-            console.log("Quedaron empate");
-            return;
-        }else if (nuevaCarrera._tortuga >= 90){
-            console.log("Gano la tortuga");
-            return;
-        }else if(nuevaCarrera._liebre >= 90){
-            console.log("Gano la liebre");
-            return;
+        }else if(avanceLiebre >= 19 && avanceLiebre <= 36){
+            this._posicion += 9;
+
+        }else if(avanceLiebre >= 37 && avanceLiebre <= 45){
+            this._posicion -= 12;
+
+        }else if(avanceLiebre >= 46 && avanceLiebre <= 76){
+            this._posicion += 1;
+        
+        }else{
+            this._posicion -= 2;
         }
     }
+}
+
+let pancho = new Tortuga();
+let paco = new Liebre();
+
+while(pancho.posicion<90 && paco.posicion<90){
+    pancho.avanzar();
+    paco.avanzar();
+    console.log("Tortuga: "+pancho.posicion+" --- "+"Liebre: "+paco.posicion);
+}
+
+if(pancho.posicion >= 90 && paco.posicion >= 90){
+    console.log("Quedaron empate");
+}else if (pancho.posicion >= 90){
+    console.log("Gano la tortuga");
+}else{
+    console.log("Gano la liebre");
 }
